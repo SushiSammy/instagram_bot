e3037113aa3e649d269b86c8e9b12dbf4e0a00c2
sam <sshxing@gmail.com>
2024-01-13 00:32:36 -0800

    updated gitignore, file names, testing publitio api using red panda pics

--- publito.py
+++ publito.py
@@ -1,2 +1,9 @@
-import secrets
+from constants import *
 from publitio import PublitioAPI
+
+publitio_api = PublitioAPI(key=PUBLITIO_API_KEY, secret=PUBLITIO_API_SECRET)
+
+with open('/Users/samuelxing/instagram_bot/red_panda_test.jpeg', 'rb') as f:
+    result = publitio_api.create_file(file=f, title='test title', description='test description')
+
+print(result)
