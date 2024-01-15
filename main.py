67d17abba92ffd17780e2c5a8f34d18833e97833
sam <sshxing@gmail.com>
2024-01-14 16:50:57 -0800

    deleted some files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from publitio_upload import *
 from instagram_upload import upload_to_instagram
+import time
 
 def main():
     # Generate video
@@ -7,15 +8,17 @@ def main():
 
     # Upload video to publitio
     publitio_response = upload_to_publitio('/Users/samuelxing/instagram_bot/test_vid.mp4')
-
+    print(publitio_response)
     # Upload video to instagram
     video_url = publitio_response['url_short']
+    time.sleep(240)
+    print(video_url)
     upload_to_instagram(video_url)
     # test url: "https://media.publit.io/file/test-vid-3.mp4"
 
     # Delete video from publitio
-    video_id = publitio_response['id']
-    delete_from_publitio(video_id)
+    # video_id = publitio_response['id']
+    # delete_from_publitio(video_id)
 
 
 
